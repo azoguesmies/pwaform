13938d2e8bf46f3b67646c91da24781388cff4d4
azoguesmies <azoguesmies@gmail.com>
2026-06-10 13:02:52 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,10 +5,24 @@ const FILES = [
   '/icon-192.svg', '/icon-512.svg'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+//self.addEventListener('install', e => {
+  //e.waitUntil(
+    //caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+  //);
+//});
+
+// Instalación del Service Worker
+self.addEventListener('install', event => {
+  console.log('Service Worker: Instalando...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Service Worker: Cache abierto');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(err => console.error('Error en cache:', err))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
